804ccd666b5e80e1bd822cf67774969ef5f3eb1b
Scott Bakker <scott@bakkergames.com>
2018-07-13 22:41:53 -0400

    kbd push pop

--- Source/IDRIS.Runtime/IDRIS.Runtime/Keyboard.cs
+++ Source/IDRIS.Runtime/IDRIS.Runtime/Keyboard.cs
@@ -1,14 +1,32 @@
 ﻿// Keyboard.cs - 07/13/2018
 
+using System;
+using System.Collections;
+
 namespace IDRIS.Runtime
 {
     public static partial class Keyboard
     {
+        private static Queue _kbd = new Queue();
         private static bool _klock = false;
 
         public static void KeyLock(bool value)
         {
             _klock = value;
+        }
+
+        public static void AddChar(char c)
+        {
+            _kbd.Enqueue(c);
+        }
+
+        public static char GetChar()
+        {
+            if (_kbd.Count <=0)
+            {
+                throw new SystemException("keyboard buffer underflow");
+            }
+            return (char)_kbd.Dequeue();
         }
     }
 }
